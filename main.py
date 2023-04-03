0821394d335d018c318c0c84049b82a62a84a4af
Rouela D <81519103+angelorenz70@users.noreply.github.com>
2023-04-03 14:11:48 +0800

    done

--- main.py
+++ main.py
@@ -31,12 +31,29 @@ def predict():
         6: 'Other'
     }
 
+
+
     # convert predicted probabilities to labels using label_map
     y_pred = [label_map[pred] for pred in np.argmax(y_prediction, axis=1)]
 
+    p1 = float(y_prediction[0][0]) * 100
+    p2 = float(y_prediction[0][1]) * 100
+    p3 = float(y_prediction[0][2]) * 100
+    p4 = float(y_prediction[0][3]) * 100
+    p5 = float(y_prediction[0][4]) * 100
+    p6 = float(y_prediction[0][5]) * 100
+    p7 = float(y_prediction[0][6]) * 100
 
     # Return the prediction as a JSON response
-    return jsonify({'prediction': str(y_pred[0])})
+    return jsonify({'prediction': str(y_pred[0]),
+                    'prob1': "{:.2f}".format(p1),
+                    'prob2': "{:.2f}".format(p2),
+                    'prob3': "{:.2f}".format(p3),
+                    'prob4': "{:.2f}".format(p4),
+                    'prob5': "{:.2f}".format(p5),
+                    'prob6': "{:.2f}".format(p6),
+                    'prob7': "{:.2f}".format(p7)
+                    })
 
 if __name__ == '__main__':
     app.run(debug=True)
